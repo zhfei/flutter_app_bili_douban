400cb32c436e97fe46d940b2e3b476c03c25eb65
周飞 <1174977961@qq.com>
2020-05-03 19:03:36 +0800

    json转model，面向对象思维编程

---
+++ lib/modles/home_modle.dart
@@ -0,0 +1,99 @@
+
+class MovieRating {
+  String stars;
+  int max;
+  int min;
+  double average;
+
+  MovieRating.fromMap(Map<String, dynamic> json) {
+    this.stars = json["stars"];
+    this.max = json["max"];
+    this.min = json["min"];
+    this.average = json["average"];
+  }
+
+}
+
+
+class MovieAvatar {
+  String small;
+  String large;
+  String medium;
+
+  MovieAvatar.fromMap(Map<String, dynamic> json) {
+    this.small = json["small"];
+    this.large = json["large"];
+    this.medium = json["medium"];
+  }
+}
+
+class MovieCast {
+  String id;
+  String alt;
+  String name;
+  String name_en;
+  MovieAvatar avatars;
+
+  MovieCast.fromMap(Map<String, dynamic> json) {
+    this.name = json["name"];
+    this.id = json["id"];
+    this.alt = json["alt"];
+    this.name_en = json["name_en"];
+    this.avatars = MovieAvatar.fromMap(json["avatars"]);
+  }
+}
+
+class MovieItem {
+  String title;
+  String id;
+  String alt;
+  String year;
+  String subtype;
+  String original_title;
+
+  String mainland_pubdate;
+  int collect_count;
+  bool has_video;
+  MovieAvatar images;
+  MovieRating rating;
+
+  List<dynamic> genres;
+  List<dynamic> durations;
+  List<dynamic> pubdates;
+
+  List<MovieCast> casts;
+  List<MovieCast> directors;
+
+
+  MovieItem.fromMap(Map<String, dynamic> json) {
+    this.title = json["title"];
+    this.id = json["id"];
+    this.alt = json["alt"];
+    this.year = json["year"];
+    this.subtype = json["subtype"];
+    this.original_title = json["original_title"];
+
+    this.mainland_pubdate = json["mainland_pubdate"];
+    this.collect_count = json["collect_count"];
+    this.has_video = json["has_video"];
+    this.images = MovieAvatar.fromMap(json["images"]);
+    this.rating = MovieRating.fromMap(json["rating"]);
+
+    this.genres = json["genres"];
+    this.durations = json["durations"];
+    this.pubdates = json["pubdates"];
+
+    this.casts = [];
+    List castList = json["casts"];
+    for (int i = 0; i< castList.length; i++) {
+      this.casts.add(MovieCast.fromMap(castList[i]));
+    }
+
+    this.directors = [];
+    List directorList = json["directors"];
+    for (int i = 0; i< castList.length; i++) {
+      this.directors.add(MovieCast.fromMap(castList[i]));
+    }
+  }
+}
+
--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -1,6 +1,7 @@
 
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app_bili_douban/modles/home_modle.dart';
 import 'package:flutter_app_bili_douban/network/HttpRequest.dart';
 
 class Home extends StatelessWidget {
@@ -21,6 +22,7 @@ class HomeBody extends StatefulWidget {
 }
 
 class _HomeBodyState extends State<HomeBody> {
+  List <MovieItem> items = [];
   
   @override
   void initState() {
@@ -30,18 +32,32 @@ class _HomeBodyState extends State<HomeBody> {
     /// 在initState方法中发生网络请求
     HttpRequest.request("https://douban.uieee.com/v2/movie/top250?start=0&count=20").then((response) {
 
-      final subjects = response.data["subjects"];
+      final List subjects = response.data["subjects"];
 
-      print("res: ${subjects}");
-      print("resType: ${subjects.runtimeType}");
+      List <MovieItem> netList = [];
+
+      for(int i = 0; i< subjects.length; i++) {
+        netList.add(MovieItem.fromMap(subjects[i]));
+      }
+
+      setState(() {
+        this.items = netList;
+      });
+
+      print("res: ${items}");
+      print("resType: ${items.runtimeType}");
 
     });
   }
   
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("首页", style: TextStyle(fontSize: 30),),
+    return ListView.builder(itemBuilder: (BuildContext, index) {
+      MovieItem item = this.items[index];
+      return ListTile(leading: Image.network(item.images.large),title: Text(item.title),
+      );
+    },
+      itemCount: this.items.length,
     );
   }
 }
